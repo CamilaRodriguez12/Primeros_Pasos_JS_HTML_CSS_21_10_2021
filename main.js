f96f2c03fab96fd5977b8c803bbe9e8d7f15a014
CamilaRodriguez12 <camii.rodrii@gmail.com>
2021-10-21 21:34:53 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+console.log("Hola Mundo :) "); // Para imprimir en la consola
+
+// Dato string
+let nombre = "Camila Rodríguez"; // let es para crear una variable global
+console.log(nombre); // Imprimir
+console.log(Object(nombre)); // Imprime que tipo de variable es
+
+// Dato Number
+let edad = 24;
+console.log(edad); 
+console.log(Object(edad));
+
+// Dato Number
+let altura = 1.69;
+console.log(altura);
+console.log(Object(altura));
+
+// Dato boolean
+let soyProfesor = false;
+console.log(soyProfesor);
+console.log(Object(soyProfesor));
+
+// Dato null
+let cabello = null; // cuando no se sabe un dato exacto
+console.log(cabello);
+console.log(Object(cabello));
+
+// Dato undefined
+let brazos; // para asignarle un dato más adelante
+console.log(brazos);
+console.log(Object(brazos));
+
+//Datos array
+let pasatiempos = ["Dibujar", "Escuchar música"]; 
+console.log(pasatiempos);
+console.log(Object(pasatiempos));
+
+// Dato Objeto
+let direccion = {
+    carrera: "40",
+    numero: "125-43",
+    departamento: "Santander"
+};
+console.log(direccion);
+console.log(Object(direccion));
+
+
